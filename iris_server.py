b009f6c555b94718de011d234b372e9507c412bc
Christopher Bohorquez <chrisishere4@gmail.com>
2023-10-23 15:30:38 -0400

    first commit

---
+++ iris_server.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+import uvicorn
+from sklearn.datasets import load_iris
+from sklearn.naive_bayes import GaussianNB
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class request_body(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+iris = load_iris()
+
+X = iris.data
+Y = iris.target
+
+#clf stands for classifier
+clf = GaussianNB()
+clf.fit(X,Y)
+
+@app.get('/')
+def home():
+    return {"Iris Home": "Welcome!ea"}
+
+@app.post('/predit')
+def predict(data: request_body):
+    test_data = [[
+        data.sepal_length,
+        data.sepal_width,
+        data.petal_length,
+        data.petal_width
+    ]]
+
+    class_idx = clf.predict(test_data)[0]
+
+    return {'class': iris.target_names[class_idx]}
